207979b45f99c27ff42b4f0b8227a63ef54f6780
unknown <santiago.rg@live.com>
2022-10-11 22:30:29 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxGraphModule } from '@swimlane/ngx-graph';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxGraphModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
