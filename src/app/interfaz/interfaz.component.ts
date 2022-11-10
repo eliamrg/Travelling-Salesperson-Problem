402f1e7a97f92433f1d05fe1af5313d17ad7e404
unknown <santiago.rg@live.com>
2022-11-09 20:45:18 -0600

    Programa Funcional en consola

--- src/app/interfaz/interfaz.component.ts
+++ src/app/interfaz/interfaz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ResolvedReflectiveFactory } from '@angular/core';
 
 @Component({
   selector: 'app-interfaz',
@@ -55,78 +55,149 @@ export class InterfazComponent implements OnInit {
     }*/
   ];
 
-  //ALGORITMO DEL VECINO MAS CERCANO
-  //PASO 1: SE PARTE DE UN NODO ORIGEN "0"
-  //PASO 2: SE ENCUENTRA LA RUTA MAS CORTA A OTRO NODO "1" Y SE GUARDA LA DISTANCIA
-  //PASO 3: EL NODO "1" AHORA ES EL ORIGEN
-  //PASO 4: SE MARCA EL NODO ANTERIOR "0" COMO VISITADO 
-  //PASO 5: SI TODOS LOS NODOS FUERON VISITADOS SE ACABA EL PROCESO
-  //PASO 6: SI NO, VOLVER AL PASO 2
-   
-  //PASO 1
-  origen:number=0; nodoCercano:number=2; nodoAnterior=0;
-  vecinoMasCercano(){
-    this.GenerarDiagrama();
-    //DATOS DE PRUEBA
+  Resolver(/*grafo:string[][]*/origen:number){
+    let grafo:number[][];
+    
+    let vertice: Array<number>=[];
     let DistanciasPrueba= [
-        ["A", "0", "2451", "713", "1018", "1631", "1374", "2408", "213", "2571", "875", "1420", "2145", "1972" ],
-        ["B", "2451", "0", "1745", "1524", "831", "1240", "959", "2596", "403", "1589", "1374", "357", "579" ],
-        ["C", "713", "1745", "0", "355", "920", "803", "1737", "851", "1858", "262", "940", "1453", "1260" ],
-        ["D", "1018", "1524", "355", "0", "700", "862", "1395", "1123", "1584", "466", "1056", "1280", "987" ],
-        ["E", "1631", "831", "920", "700", "0", "663", "1021", "1769", "949", "796", "879", "586", "371" ],
-        ["F", "1374", "1240", "803", "862", "663", "0", "1681", "1551", "1765", "547", "225", "887", "999" ],
-        ["G", "2408", "959", "1737", "1395", "1021", "1681", "0", "2493", "678", "1724", "1891", "1114", "701" ],
-        ["H", "213", "2596", "851", "1123", "1769", "1551", "2493", "0", "2699", "1038", "1605", "2300", "2099" ],
-        ["I", "2571", "403", "1858", "1584", "949", "1765", "678", "2699", "0", "1744", "1645", "653", "600" ],
-        ["J", "875", "1589", "262", "466", "796", "547", "1724", "1038", "1744", "0", "679", "1272", "1162" ],
-        ["K", "1420", "1374", "940", "1056", "879", "225", "1891", "1605", "1645", "679", "0", "1017", "1200" ],
-        ["L", "2145", "357", "1453", "1280", "586", "887", "1114", "2300", "653", "1272", "1017", "0", "504"],
-        ["M", "1972", "579", "1260", "987", "371", "999", "701", "2099", "600", "1162", "1200", "504", "0" ]
-    ];
-    
-    
-    this.TablaDistancias=DistanciasPrueba;
-    this.Nodos=["","A","B","C","D","E","F","G","H","I","J","K","L","M",]
-    
-    //PASO 2
-    let Distancias:number[][]=this.SimplificarArreglo();
-    let DistanciasPosibles:number[][]=this.ArregloDistanciasPosibles();
-    /*for(let i=1;i<=this.nodosVisitados.length;i++){
-      console.log(this.nodosVisitados);
+      ["A", "0", "2451", "713", "1018", "1631", "1374"],
+      ["B", "2451", "0", "1745", "1524", "831", "1240"],
+      ["C", "713", "1745", "0", "355", "920", "803"],
+      ["D", "1018", "1524", "355", "0", "700", "862"],
+      ["E", "1631", "831", "920", "700", "0", "663"],
+      ["F", "1374", "1240", "803", "862", "663", "0" ],
       
-      if(this.nodosVisitados.includes(String.fromCharCode(64+i))){
-        console.log("ORIGEN:"+this.origen+ "ELIMINAR :"+String.fromCharCode(64+i))
-        DistanciasPosibles[this.origen][i-1]=999999999;
-      }
-    }console.log(DistanciasPosibles);*/
+  ];
+  
+  this.Nodos=["","A","B","C","D","E","F"]
+  
+  this.TablaDistancias=DistanciasPrueba;
+    /*grafo=[ [ 0, 10, 15, 20],
+    [ 10, 0, 35, 25 ],
+    [ 15, 35, 0, 30 ],
+    [ 20, 25, 30, 45 ] ];*/
+    grafo=this.SimplificarArreglo();
+    let V =grafo.length;
+    console.log(grafo)
+ 
+        for (let i = 0; i < V; i++)
+            if (i != origen)
+            vertice.push(i);
+ 
+        // almacenar costo minimo
+        // Ciclo hamiltoniano.
+
+        //ese es un numero muy grande xd. Nomas pa guardar el minimo al compararlo con ese
+        let distanciaMinima = Number.MAX_VALUE;
+        let rutaMinima="";
+        let ruta=1;
+        do {
+            // almacenar el costp de la ruta actual 
+            let costoRutaActual = 0;
+            let rutaActual = "A";
+            let k = origen;
+            console.log("Ruta: "+ruta)
+            // calcular el costo de la ruta actual
+            for (let i = 0; i < vertice.length; i++) {
+              console.log("Avanzar a nodo "+(vertice[i]+1)+ " Costo= "+grafo[k][vertice[i]])
+              //console.log("graph[" +(k+1)+"][vertex["+(i+1)+"]] =" +graph[k][vertex[i]])
+              rutaActual+="-"+String.fromCharCode(64+(vertice[i]+1));  
+              costoRutaActual += Number(grafo[k][vertice[i]]);
+                k = vertice[i];
+            }
+            
+            costoRutaActual += Number(grafo[k][origen]);
+            rutaActual+="-A";
+            if(costoRutaActual== Math.min(distanciaMinima, costoRutaActual)){
+              rutaMinima=rutaActual;
+            }
+            // actualizar minimo
+            distanciaMinima
+                = Math.min(distanciaMinima, costoRutaActual);
+            ruta++;
+ 
+        } while (this.findNextPermutation(vertice));
+        
+        //regresa el costo minimo;
+        console.log ("Costo mínimo: "+distanciaMinima);
+        console.log(rutaMinima)
+  }
+  findNextPermutation(datos: Array<number>)
+  {
+        // Si el conjunto de datos dado está vacío
+        // o contiene solo un elemento
+        // la siguiente permutacion no es posible
+        if (datos.length <= 1)
+            return false;
+        let last = datos.length - 2;
+ 
+        // Encontrar el sufijo no creciente más largo y el pivote
+        while (last >= 0) {
+            if (datos[last] <  datos[last + 1])
+                break;
+            last--;
+        }
+ 
+        // Si no hay par creciente no hay permutación de mayor orden
+        if (last < 0)
+            return false;
+        let nextGreater = datos.length - 1;
+ 
+        // Encontrar el sucesor más a la derecha del pivote
+        for (let i = datos.length - 1; i > last; i--) {
+            if (datos[i] >  datos[last]) {
+                nextGreater = i;
+                break;
+            }
+        }
+ 
+        
+        // Intercambiar el sucesor y el pivote
+        datos = this.swap(datos, nextGreater, last);
+ 
+        //Invertir el sufijo
+
+        datos = this.reverse(datos, last + 1, datos.length - 1);
+ 
+        // Devuelve verdadero cuando se realiza la siguiente permutación
+        return true;
+    }
+    
+    swap(datos: Array<number> , left: number, right: number)
+    {
+    // intercambiar los datos
+    let temp = datos[left];
+    datos[left] = datos[right];
+    datos[right] = temp;
+
+    // Devolver la matriz actualizada
+    return datos;
+    }
+
+    //Función para invertir el subarreglo comenzando de izquierda a derecha 
+    reverse(datos: Array<number>, left: number, right: number)
+    {
+    // Inversion del sub arreglo
+    while (left < right) {
+    let temp = datos[left];
+    datos[left++] = datos[right];
+    datos[right--] = temp;
+    }
+
+    // regresar el arreglo actualizado
+    return datos;
+    }
+
 
     
+ 
+//A PARTIR DE AQUI SON COSAS PARA LA INTERFAZ, LA LOGICA ES LO DE ARRIBA:))
 
-
-   
-    this.nodoCercano=Distancias[this.origen].indexOf(Math.min(...DistanciasPosibles[this.origen]))+1;
-    console.log(this.origen,this.nodoCercano,String.fromCharCode(65+this.origen)+" x "+String.fromCharCode(64+this.nodoCercano) + " DIST: " +this.TablaDistancias[this.origen][this.nodoCercano] );
-    this.distanciaRecorrida+=Number(this.TablaDistancias[this.origen][this.nodoCercano]);
-    //PASO 3
-    this.origen=this.nodoCercano-1;
-    //PASO 4
-    this.nodosVisitados.push(String.fromCharCode(64+this.nodoCercano));
-
-    console.log("NODO VISITADO: "+String.fromCharCode(64+this.nodoCercano));
-
-
-    //PASO 5
-    if (this.nodosVisitados.length!=this.Nodos.length-1){
-      //PASO 2
-      this.vecinoMasCercano();
-    }  
-    //console.log(this.nodosVisitados);
-  }
 
   SimplificarArreglo(){
     let arr:number[][]=[];
     for(let i=0;i<this.TablaDistancias.length;i++){
-      arr.push([]);
+      arr.push([]); 
       for(let j=1;j<this.TablaDistancias[i].length;j++){
         //console.log(i,j,this.TablaDistancias[i][j]);
         arr[i].push(Number(this.TablaDistancias[i][j]));
@@ -137,56 +208,15 @@ export class InterfazComponent implements OnInit {
     return arr;
   }
 
-  ArregloDistanciasPosibles(){
-    let arr:number[][]=[];
-    for(let i=0;i<this.TablaDistancias.length;i++){
-      arr.push([]);
-      for(let j=1;j<this.TablaDistancias[i].length;j++){
-        //console.log(i,j,this.TablaDistancias[i][j]);
-        //if(Number(this.TablaDistancias[i][j])!=0 /*&& !this.nodosVisitados.includes(String.fromCharCode(64+j))*/)
-        arr[i].push(Number(this.TablaDistancias[i][j]));
-        
-      }
-    }
-    console.log(arr);
-    return arr;
-  }
-/*do{
-        for(let i=1;i<=this.Distancias[origen].length;i++){
-          console.log("for")
-          if (i==1){
-            for(let j=1;j<=this.nodosVisitados.length;j++){
-              if(!this.nodosVisitados.includes(String.fromCharCode(64+j))){
-                nodoCercano=j;
-                break;
-              }
-            }           
-          }
-          else if( !this.nodosVisitados.includes(String.fromCharCode(64+i)) && (Number(this.Distancias[origen][nodoCercano])>Number(this.Distancias[origen][i])) && Number(this.Distancias[origen][i])!=0){
-            nodoCercano=i;
-          }
-        }
-        
-        console.log(String.fromCharCode(64+nodoCercano));
-        console.log(this.Distancias[origen][nodoCercano]);
-        this.distanciaRecorrida+=Number(this.Distancias[origen][nodoCercano]);
-        this.nodosVisitados.push(String.fromCharCode(64+nodoCercano));
-
-        origen=nodoCercano;
-    }while(this.nodosVisitados.length!=this.Nodos.length-1);
-    
-    console.log(this.nodosVisitados);
-    console.log(this.distanciaRecorrida);*/
-
   leerLinks() {
-    //let data = [this.multiple.find((e) => e.name == name)];
-    let data = this.links;
-    return data;
+    //let datos = [this.multiple.find((e) => e.name == name)];
+    let datos = this.links;
+    return datos;
   }
   leerNodos() {
-    //let data = [this.multiple.find((e) => e.name == name)];
-    let data = this.nodes;
-    return data;
+    //let datos = [this.multiple.find((e) => e.name == name)];
+    let datos = this.nodes;
+    return datos;
   }
   agregarNodo(){
     
@@ -236,3 +266,5 @@ export class InterfazComponent implements OnInit {
     
   }
 }
+
+
